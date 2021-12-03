ba8bcf3a37b4c20e6ff5c0ad931a58f2cceba44c
Hirekari-Abhishek-Raj <91818785+Hirekari-Abhishek-Raj@users.noreply.github.com>
2021-12-03 23:54:05 +0530

    Create Number occurring maximum times.py
    Given a list of N integers sorted in ascending order, Please find the number which occurs 4 times in the array
    
    Input
    First number is N (the number of integers given) Followed by the N numbers
    
    Output
    Print the number which occurs 4 times. print -1 if such a number doesnt exist
    
    Example
    Input: 10
    
    1
    
    2
    
    3
    
    4
    
    4
    
    4
    
    4
    
    5
    
    6
    
    6
    
    Output: 4

---
+++ Number occurring maximum times.py
@@ -0,0 +1,13 @@
+dict={}
+n=int(input())
+for i in range(n):
+    val=int(input())
+    if val not in dict:
+        dict[val]=1
+    else:
+        dict[val]+=1
+for i in dict.keys():
+    if dict[i]==4:
+        print(i)
+        break
+    
